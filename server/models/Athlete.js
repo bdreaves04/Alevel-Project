ec0ca1b3657a0f7addf49339b0e4879fe98abb2c
Ben <bdreaves04@gmail.com>
2022-09-07 21:40:44 +0100

    creates timestamps on any changes to db

--- server/models/Athlete.js
+++ server/models/Athlete.js
@@ -2,25 +2,28 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const athleteSchema = new Schema({
-  forename: {
-    type: String,
-    required: true,
+const athleteSchema = new Schema(
+  {
+    forename: {
+      type: String,
+      required: true,
+    },
+    surname: {
+      type: String,
+      required: true,
+    },
+    beltClass: {
+      type: String,
+      required: true,
+    },
+    weightClass: {
+      type: String,
+      required: true,
+    },
+    madeWeight: {
+      type: Boolean,
+      default: false,
+    },
   },
-  surname: {
-    type: String,
-    required: true,
-  },
-  beltClass: {
-    type: String,
-    required: true,
-  },
-  weightClass: {
-    type: String,
-    required: true,
-  },
-  madeWeight: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
