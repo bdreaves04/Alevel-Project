8f8eacbb0ef7715a59d32ef93d10e79cbbc279be
Ben <bdreaves04@gmail.com>
2022-09-07 21:36:41 +0100

    formatting (minor)

--- server/models/Athlete.js
+++ server/models/Athlete.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const athleteSchema = new Schema({
-    forename: {
-        type: String,
-        required: true
-    },
-    surname: {
-        type: String,
-        required: true
-    },
-    beltClass: {
-        type: String,
-        required: true
-    },
-    weightClass: {
-        type: String,
-        required: true
-    },
-    madeWeight: {
-        type: Boolean
-    }
-})
+  forename: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  beltClass: {
+    type: String,
+    required: true,
+  },
+  weightClass: {
+    type: String,
+    required: true,
+  },
+  madeWeight: {
+    type: Boolean,
+  },
+});
