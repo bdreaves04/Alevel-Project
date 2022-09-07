c2d8b3887416eb0f0cf4bda8680f4da3900fcf32
Ben <bdreaves04@gmail.com>
2022-09-07 21:08:28 +0100

    allows use of body of requests in apis

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const athleteRoutes = require('./routes/athletes')
 //creates express app
 const app = express()
 
+
+app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
