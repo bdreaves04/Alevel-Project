eca78743045a81fa247db795588d6e49e1264ad1
Ben <bdreaves04@gmail.com>
2022-09-07 21:36:02 +0100

    defined Schema for athletes in db

---
+++ server/models/Athlete.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+const athleteSchema = new Schema({
+    forename: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String,
+        required: true
+    },
+    beltClass: {
+        type: String,
+        required: true
+    },
+    weightClass: {
+        type: String,
+        required: true
+    },
+    madeWeight: {
+        type: Boolean
+    }
+})
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const athleteRoutes = require("./routes/athletes");
+const athleteRoutes = require("./routes/Athletes");
 
 //creates express app
 const app = express();
