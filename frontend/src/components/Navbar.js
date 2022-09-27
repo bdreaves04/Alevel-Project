deaf2554a9d60e949711e35f996df9f048a42f4a
Ben <bdreaves04@gmail.com>
2022-09-27 12:32:13 +0100

    added logo to navbar and to tab icon

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
 class Navbar extends Component {
     render() {
         return (
             <header>
                 <div className="containerNav">
+                    <div className="logoNavItem">
+                        <img src="./logo.svg" alt="logo" />
+                    </div>
                     <Link to='/'>
                         <div className="navitem">
                             <h2>Home</h2>
