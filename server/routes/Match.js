eb3ffe5080dff8bd987158f282985360b55e39a8
Ben <bdreaves04@gmail.com>
2022-10-14 12:12:00 +0100

    added bcrypt:

---
+++ server/routes/Match.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { getMatches, getMatchByRing, getMatchByMatchNo, createMatch, updateMatch } = require('../controllers/matchesController');
+const router = express.Router();
+
+//get all matches
+router.get('/', getMatches);
+
+//get matches by ring
+router.post('/ring/', getMatchByRing);
+
+//get match by matchNo
+router.post('/getMatchFromNo/', getMatchByMatchNo);
+
+//create a match
+router.post('/', createMatch);
+
+//update a match by matchNo
+router.put('/', updateMatch);
+
+module.exports = router;
