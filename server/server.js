1bf8f68da0ef0806366e735e5fe3fc8a4b23f7af
Ben <bdreaves04@gmail.com>
2022-10-14 12:12:00 +0100

    added user routes and changed a few api routes since get requests cant have a body

--- server/server.js
+++ server/server.js
@@ -8,18 +8,13 @@ const athleteRoutes = require("./routes/Athletes");
 const athleteRoutes = require("./routes/Athlete");
 const matchRoutes = require("./routes/Match");
 const userRoutes = require("./routes/User");
->>>>>>> dafe083 (added user routes and changed a few api routes since get requests cant have a body)
 
 //creates express app
 const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-<<<<<<< HEAD
-  console.log(req.path, req.method);
-=======
   // console.log(req.path, req.method, '\n', req.body);
->>>>>>> dafe083 (added user routes and changed a few api routes since get requests cant have a body)
   next();
 });
 
@@ -29,7 +24,6 @@ app.use("/api/athletes", athleteRoutes);
 =======
 app.use("/api/matches", matchRoutes);
 app.use("/api/user", userRoutes);
->>>>>>> dafe083 (added user routes and changed a few api routes since get requests cant have a body)
 
 //connect to db
 mongoose
