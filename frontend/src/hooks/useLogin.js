9c7d8e8d78fac854c27d1c10456a3cf9f0cc7496
Ben <bdreaves04@gmail.com>
2022-12-05 20:19:54 +0000

    added login logic similar to signup

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogin = () => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    const { dispatch } = useAuthContext();
+
+    const login = async (username, password) => {
+        setIsLoading(true);
+        setError(null);
+
+        await fetch("/api/user/login", {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({ username, password }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (!data.ok) {
+                    setIsLoading(false);
+                    setError(data.error);
+                } else {
+                    localStorage.setItem("user", JSON.stringify(data));
+
+                    dispatch({ type: "LOGIN", payload: data });
+                    setIsLoading(false);
+                }
+            });
+    };
+
+    return { login, isLoading, error };
+};
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,19 +1,53 @@
-import React, { Component } from 'react';
-import Card from 'react-bootstrap/Card';
-class Login extends Component {
-    render() {
-        document.title='Login'
-        return (
-            <div className="login">
-                <Card>
-                    <Card.Body>
-                        <Card.Title>Login Page</Card.Title>
-                        <Card.Text>here will go the card body</Card.Text>
-                    </Card.Body>
-                </Card>
-            </div>
-        );
-    }
-}
+import { useState } from "react";
+import { Card, Form, Button } from "react-bootstrap";
+import { useLogin } from "../hooks/useLogin";
+
+const Login = () => {
+    document.title = "Login";
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const { login, error, isLoading } = useLogin();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        await login(username, password);
+    };
+
+    return (
+        <div className="login">
+            <Card>
+                <Card.Body>
+                    <Card.Title>Login Page</Card.Title>
+                    <Form onSubmit={handleSubmit()}>
+                        <Form.Group>
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control
+                                type="username"
+                                onChange={(e) => setUsername(e.target.value)}
+                                placeholder="Enter Username"
+                                value={username}
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder="Enter Passsword"
+                                value={password}
+                            />
+                        </Form.Group>
+                        <br />
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                        {error && <div className="error">{error}</div>}
+                    </Form>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
 
 export default Login;
