fda7946df87d692b2022c3314cca5795c286b8c6
Ben <bdreaves04@gmail.com>
2022-12-06 10:16:17 +0000

    changed navbar to display logout button when logged in and login/signup pages when logged out

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,38 +1,52 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { useLogout } from '../hooks/useLogout';
-class Navbar extends Component {
-    render() {
-        return (
-            <header>
-                <div className="containerNav">
-                    <div className="logoNavItem">
-                        <img src="./logo.svg" alt="logo" />
-                    </div>
-                    <Link to='/'>
-                        <div className="navitem">
-                            <h2>Home</h2>
-                        </div>
-                    </Link>
-                    <Link to='/nextMatches'>
-                        <div className="navitem">
-                            <h2>Next Matches</h2>
-                        </div>
-                    </Link>
-                    <Link to='/login'>
-                        <div className="navitem">
-                            <h2>Login</h2>
-                        </div>
-                    </Link>
-                    <Link to='/signup'>
-                        <div className="navitem">
-                            <h2>Signup</h2>
-                        </div>
-                    </Link>
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
+import { Button } from "react-bootstrap";
+
+const Navbar = () => {
+    const {user} = useAuthContext();
+    const { logout } = useLogout()
+
+    return (
+        <header>
+            <div className="containerNav">
+                <div className="logoNavItem">
+                    <img src="./logo.svg" alt="logo" />
                 </div>
-            </header>
-        );
-    }
-}
+                <Link to="/">
+                    <div className="navitem">
+                        <h2>Home</h2>
+                    </div>
+                </Link>
+                <Link to="/nextMatches">
+                    <div className="navitem">
+                        <h2>Next Matches</h2>
+                    </div>
+                </Link>
+                {!user && (
+                    <>
+                        <Link to="/login">
+                            <div className="navitem">
+                                <h2>Login</h2>
+                            </div>
+                        </Link>
+                        <Link to="/signup">
+                            <div className="navitem">
+                                <h2>Signup</h2>
+                            </div>
+                        </Link>
+                    </>
+                )}
+                {user && (
+                    <>
+                        <Button onClick={logout}>
+                            Logout
+                        </Button>
+                    </>
+                )}
+            </div>
+        </header>
+    );
+};
 
 export default Navbar;
