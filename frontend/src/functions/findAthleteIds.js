e8eacedc3be469823368053da21310c88fb692e0
Ben <bdreaves04@gmail.com>
2023-02-02 09:57:09 +0000

    removed redundant temporary variable

--- frontend/src/functions/findAthleteIds.js
+++ frontend/src/functions/findAthleteIds.js
@@ -1,15 +1,14 @@
 export const findId = async (athleteNo) => {
-    const athleteID = await fetch("/api/athletes/getByAthleteNo", {
+    await fetch("/api/athletes/getByAthleteNo", {
         method: "POST",
         headers: { "Content-type": "application/json" },
         body: JSON.stringify({ athleteNo: athleteNo }),
     })
         .then((res) => res.json())
         .then((data) => {
-            if(data.error){
-                return data.error
+            if (data.error) {
+                return data.error;
             }
             return data._id;
         });
-    return athleteID;
 };
