88cb50994b534c00d4df5cd210478fd34fa4f9c5
Ben <bdreaves04@gmail.com>
2023-02-06 14:36:33 +0000

    added rows/columns to referee pages

--- frontend/src/pages/RefereePagesContents.js
+++ frontend/src/pages/RefereePagesContents.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const RefereePagesContents = () => {
     document.title = "Referee Contents";
@@ -8,7 +10,19 @@ const RefereePagesContents = () => {
             <Card>
                 <Card.Body>
                     <Card.Title>Referee Pages</Card.Title>
-                    <Card.Text></Card.Text>
+                    <Row>
+                        <Col>
+                            <Link to={"/ringSide"}>Ring Side</Link>
+                        </Col>
+                        <Col>
+                            <Link to={"/matchNo"}>MatchNoScreen</Link>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Link to={"/checkIn"}>CheckIn Desk</Link>
+                        </Col>
+                    </Row>
                 </Card.Body>
             </Card>
         </div>
