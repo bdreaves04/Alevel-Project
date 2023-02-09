2f7bc9f4f2e7c660f619c40ee66a79fc89eb0631
Ben <bdreaves04@gmail.com>
2023-02-09 10:09:39 +0000

    added match number screen for on ringside for audience to see current match

--- frontend/src/pages/referee pages/MatchNoScreen.js
+++ frontend/src/pages/referee pages/MatchNoScreen.js
@@ -1,12 +1,60 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React from "react";
+import { Form } from "react-bootstrap";
 
 const MatchNoScreen = () => {
-  return (
-    <Card>
-      matchNo Screen
-    </Card>
-  )
-}
+    const [ringNo, setRingNo] = React.useState(1);
+    const [matches, setMatches] = React.useState(null);
 
-export default MatchNoScreen
+    React.useEffect(() => {
+        const fetchMatches = async (ringNo) => {
+            await fetch(`/api/matches/getComplete`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ ringNo: ringNo }),
+            })
+                .then((res) => res.json())
+                .then((data) => setMatches(data));
+        };
+        fetchMatches(ringNo);
+        const interval = setInterval(() => {
+            fetchMatches(ringNo)
+        }, 10000);
+
+        return () => clearInterval(interval)
+    }, [ringNo]);
+
+    const spanStyle = {
+        color: "white",
+        fontSize: "55vw",
+    };
+
+    return (
+        <>
+            <Form.Select
+                value={ringNo}
+                onChange={(e) => {
+                    setRingNo(e.target.value);
+                }}
+            >
+                <option value={1}>ring 1</option>
+                <option value={2}>ring 2</option>
+                <option value={3}>ring 3</option>
+            </Form.Select>
+            <div
+                style={{
+                    minHeight: "100vh",
+                    minWidth: "100vw",
+                    backgroundColor: "black",
+                }}
+            >
+                {matches && (
+                    <span key={matches[0].matchNo} style={spanStyle}>
+                        {matches[0].matchNo}
+                    </span>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default MatchNoScreen;
