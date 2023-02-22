0574bc5abc49a9b284da0c79ca531c47eabca43d
Ben <bdreaves04@gmail.com>
2023-02-22 14:54:55 +0000

    added per ring fetching for useFetchMatches hook

--- frontend/src/hooks/useFetchMatches.js
+++ frontend/src/hooks/useFetchMatches.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 export const useFetchMatches = () => {
-    const [matches, setMatches] = React.useState([]);
+    const [matches, setMatches] = React.useState(null);
+    const [ringOne, setRingOne] = React.useState(null);
+    const [ringTwo, setRingTwo] = React.useState(null);
+    const [ringThree, setRingThree] = React.useState(null);
 
-    const fetchData = async (ringNo) => {
+    const fetchMatches = async (ringNo) => {
         await fetch(`/api/matches/getComplete/`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -15,5 +18,44 @@ export const useFetchMatches = () => {
             });
     };
 
-    return { matches, fetchData };
+    const fetchMatchesOne = async () => {
+        await fetch(`/api/matches/getComplete`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ ringNo: 1 }),
+        })
+            .then((res) => res.json())
+            .then((data) => setRingOne(data));
+    };
+
+    const fetchMatchesTwo = async () => {
+        await fetch(`/api/matches/getComplete`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ ringNo: 2 }),
+        })
+            .then((res) => res.json())
+            .then((data) => setRingTwo(data));
+    };
+
+    const fetchMatchesThree = async () => {
+        await fetch(`/api/matches/getComplete`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ ringNo: 3 }),
+        })
+            .then((res) => res.json())
+            .then((data) => setRingThree(data));
+    };
+
+    return {
+        matches,
+        ringOne,
+        ringThree,
+        ringTwo,
+        fetchMatchesOne,
+        fetchMatchesThree,
+        fetchMatchesTwo,
+        fetchMatches,
+    };
 };
