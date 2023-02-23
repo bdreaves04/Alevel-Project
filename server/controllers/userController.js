4928bc7cddb6bd26f58ff2be2e8ef8af37f9e421
Ben <bdreaves04@gmail.com>
2023-02-23 15:10:34 +0000

    added verification user changing details is same user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -37,6 +37,26 @@ const signupUser = async (req, res) => {
 
 const changeUsername = async (req, res) => {
     const { oldUser, newUser } = req.body;
+
+    //verifies that the user sending request is the same as the user to be changed
+    try {
+        const { authorisation } = req.headers;
+        if (!authorisation) {
+            return res
+                .status(401)
+                .json({ error: "Authorisation token required" });
+        }
+
+        const token = authorisation.split(" ")[1];
+
+        const { _id } = jwt.verify(token, process.env.SECRET);
+        const check = await userModel.findById(_id);
+
+        if (check.username !== oldUser) throw Error("not your username");
+    } catch (error) {
+        return res.status(401).json({ error: error.message });
+    }
+
     try {
         const user = await userModel.findOneAndUpdate(
             { username: oldUser },
@@ -45,14 +65,37 @@ const changeUsername = async (req, res) => {
         );
         const token = createToken(user._id, user.isAdmin);
 
-        res.status(200).json({ username: user.username, token, isAdmin: user.isAdmin });
+        res.status(200).json({
+            username: user.username,
+            token,
+            isAdmin: user.isAdmin,
+        });
     } catch (error) {
-        res.status(400).json({ error: error.message });
+        return res.status(400).json({ error: error.message });
     }
 };
 
 const changeUserPassword = async (req, res) => {
     const { username, passwordOld, passwordNew } = req.body;
+
+    //verifies that the user sending request is the same as the user to be changed
+    try {
+        const { authorisation } = req.headers;
+        if (!authorisation) {
+            return res
+                .status(401)
+                .json({ error: "Authorisation token required" });
+        }
+
+        const token = authorisation.split(" ")[1];
+
+        const { _id } = jwt.verify(token, process.env.SECRET);
+        const check = await userModel.findById(_id);
+
+        if (check.username !== username) throw Error("not your username");
+    } catch (error) {
+        return res.status(401).json({ error: error.message });
+    }
 
     try {
         const user = await userModel.changePassword(
