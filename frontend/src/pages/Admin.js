a29f91eeea26bfae1e9ff987cb7d422acbf63d96
Ben <bdreaves04@gmail.com>
2023-03-05 15:49:58 +0000

    ui improvements for mobile & formatting

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -7,37 +7,37 @@ import CreateAthleteCard from "../components/AdminPage/CreateAthleteCard";
 import UpdateAthleteCard from "../components/AdminPage/UpdateAthleteCard";
 
 const Admin = () => {
-    return (
-        <>
-            <Container fluid>
-                <Row>
-                    <Col>
-                        <Card className="adminCards overflow-auto">
-                            <CreateMatchCard />
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card className="adminCards overflow-auto">
-                            <CreateAthleteCard />
-                        </Card>
-                    </Col>
-                </Row>
-                <br id="adminCardBreak"/>
-                <Row>
-                    <Col>
-                        <Card className="adminCards overflow-auto">
-                            <UpdateMatchCard />
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card className="adminCards overflow-auto">
-                            <UpdateAthleteCard />
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col>
+            <Card className="adminCards overflow-auto">
+              <CreateMatchCard />
+            </Card>
+          </Col>
+          <Col>
+            <Card className="adminCards overflow-auto">
+              <CreateAthleteCard />
+            </Card>
+          </Col>
+        </Row>
+        <br id="adminCardBreak" />
+        <Row>
+          <Col>
+            <Card className="adminCards overflow-auto">
+              <UpdateMatchCard />
+            </Card>
+          </Col>
+          <Col>
+            <Card className="adminCards overflow-auto">
+              <UpdateAthleteCard />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Admin;
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -3,21 +3,47 @@ import { Card, Col, Row } from "react-bootstrap";
 import ChangePassword from "../components/userPage/ChangePassword";
 import ChangeUsername from "../components/userPage/ChangeUsername";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { useViewport } from "../hooks/useViewport";
 
 export const Profile = () => {
-    const { user } = useAuthContext();
+  const { width, height } = useViewport();
 
-    return (
-        <Card style={{ padding: "0.5rem", height: "90vh" }}>
-            <h1>Hello, {user.username}</h1>
-            <Row>
-                <Col>
-                    <ChangeUsername />
-                </Col>
-                <Col>
-                    <ChangePassword />
-                </Col>
-            </Row>
-        </Card>
-    );
+  return width <= 1000 ? <SmallDevice /> : <LargeDevice />;
+};
+
+const SmallDevice = () => {
+  const { user } = useAuthContext();
+  return (
+    <Card style={{ padding: "0.5rem", height: "90vh" }}>
+      <h2>Hello, {user.username}</h2>
+
+      <div className="mx-auto" style={{ width: "85vw" }}>
+        <Row>
+          <ChangeUsername />
+        </Row>
+        <div style={{ height: "1.5vh" }} />
+        <Row>
+          <ChangePassword />
+        </Row>
+      </div>
+    </Card>
+  );
+};
+
+const LargeDevice = () => {
+  const { user } = useAuthContext();
+  return (
+    <Card style={{ padding: "0.5rem", height: "90vh" }}>
+      <h1>Hello, {user.username}</h1>
+
+      <Row>
+        <Col>
+          <ChangeUsername className="h-50" />
+        </Col>
+        <Col>
+          <ChangePassword className="h-50" />
+        </Col>
+      </Row>
+    </Card>
+  );
 };
