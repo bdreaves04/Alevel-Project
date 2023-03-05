c39c68881cc8800e798cb1408e18f5585522bfb6
Ben <bdreaves04@gmail.com>
2023-03-05 18:01:33 +0000

    formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,105 +17,71 @@ import { WeighIn } from "./pages/referee pages/WeighIn";
 import { Profile } from "./pages/Profile";
 
 const App = () => {
-    const { user } = useAuthContext();
-    let loggedIn = false;
-    let adminCheck = false;
+  const { user } = useAuthContext();
+  let loggedIn = false;
+  let adminCheck = false;
 
-    if (user) {
-        loggedIn = true;
-    }
-    if (user && user.isAdmin) {
-        adminCheck = true;
-    }
+  if (user) {
+    loggedIn = true;
+  }
+  if (user && user.isAdmin) {
+    adminCheck = true;
+  }
 
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <PageNavbar />
-                <div className="pages">
-                    <Routes>
-                        <Route
-                            path="/"
-                            element={
-                                loggedIn ? <Home /> : <Navigate to="/login" />
-                            }
-                        />
-                        <Route
-                            path="/profile"
-                            element={
-                                loggedIn ? <Profile /> : <Navigate to="/login" />
-                            }
-                        />
-                        <Route
-                            path="/login"
-                            element={
-                                !loggedIn ? <Login /> : <Navigate to="/" />
-                            }
-                        />
-                        <Route
-                            path="/signup"
-                            element={
-                                !loggedIn ? <Signup /> : <Navigate to="/" />
-                            }
-                        />
-                        <Route path="/nextMatches" element={<NextMatches />} />
-                        <Route
-                            path="/admin"
-                            element={
-                                adminCheck ? <Admin /> : <Navigate to="/" />
-                            }
-                        />
-                        <Route
-                            path="/refereeContents"
-                            element={
-                                adminCheck ? (
-                                    <RefereePagesContents />
-                                ) : (
-                                    <Navigate to="/" />
-                                )
-                            }
-                        />
-                        <Route
-                            path="/checkIn"
-                            element={
-                                adminCheck ? (
-                                    <CheckInDesk />
-                                ) : (
-                                    <Navigate to="/" />
-                                )
-                            }
-                        />
-                        <Route
-                            path="/ringSide"
-                            element={
-                                adminCheck ? <RingSide /> : <Navigate to="/" />
-                            }
-                        />
-                        <Route
-                            path="/MatchNo"
-                            element={
-                                adminCheck ? (
-                                    <MatchNoScreen />
-                                ) : (
-                                    <Navigate to="/" />
-                                )
-                            }
-                        />
-                        <Route
-                            path="/weighIn"
-                            element={
-                                adminCheck ? (
-                                    <WeighIn />
-                                ) : (
-                                    <Navigate to="/" />
-                                )
-                            }
-                        />
-                    </Routes>
-                </div>
-            </BrowserRouter>
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <PageNavbar />
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={loggedIn ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/profile"
+              element={loggedIn ? <Profile /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!loggedIn ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/signup"
+              element={!loggedIn ? <Signup /> : <Navigate to="/" />}
+            />
+            <Route path="/nextMatches" element={<NextMatches />} />
+            <Route
+              path="/admin"
+              element={adminCheck ? <Admin /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/refereeContents"
+              element={
+                adminCheck ? <RefereePagesContents /> : <Navigate to="/" />
+              }
+            />
+            <Route
+              path="/checkIn"
+              element={adminCheck ? <CheckInDesk /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/ringSide"
+              element={adminCheck ? <RingSide /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/MatchNo"
+              element={adminCheck ? <MatchNoScreen /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/weighIn"
+              element={adminCheck ? <WeighIn /> : <Navigate to="/" />}
+            />
+          </Routes>
         </div>
-    );
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
