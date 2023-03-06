36228716466ec2f7119d3b22dc3823e1a4c3dc57
Ben <bdreaves04@gmail.com>
2023-03-06 12:15:46 +0000

    changed ui for admin page on smaller devices

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -8,35 +8,32 @@ import UpdateAthleteCard from "../components/AdminPage/UpdateAthleteCard";
 
 const Admin = () => {
   return (
-    <>
-      <Container fluid>
-        <Row>
-          <Col>
-            <Card className="adminCards overflow-auto">
-              <CreateMatchCard />
-            </Card>
-          </Col>
-          <Col>
-            <Card className="adminCards overflow-auto">
-              <CreateAthleteCard />
-            </Card>
-          </Col>
-        </Row>
-        <br id="adminCardBreak" />
-        <Row>
-          <Col>
-            <Card className="adminCards overflow-auto">
-              <UpdateMatchCard />
-            </Card>
-          </Col>
-          <Col>
-            <Card className="adminCards overflow-auto">
-              <UpdateAthleteCard />
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Container>
+      <Row>
+        <Col>
+          <Card className="adminCards overflow-auto">
+            <CreateMatchCard />
+          </Card>
+        </Col>
+        <Col>
+          <Card className="adminCards overflow-auto">
+            <CreateAthleteCard />
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card className="adminCards overflow-auto">
+            <UpdateMatchCard />
+          </Card>
+        </Col>
+        <Col>
+          <Card className="adminCards overflow-auto">
+            <UpdateAthleteCard />
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
